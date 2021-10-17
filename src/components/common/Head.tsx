c1a0788903fc30008dc32b50e70f2e5256b7a405
Edgar Herrera <edgarherrera@hey.com>
2021-10-16 19:13:10 -0500

    Add head component

---
+++ src/components/common/Head.tsx
@@ -0,0 +1,14 @@
+import NextHead from "next/head";
+
+export const Head = () => {
+  return (
+    <>
+      <NextHead>
+        <title>Planets</title>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#070724" />
+        <link rel="icon" href="/favicon.ico" />
+      </NextHead>
+    </>
+  );
+};
