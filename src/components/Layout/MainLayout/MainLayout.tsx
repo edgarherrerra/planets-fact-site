7d9f30a8f44e3738a3b50cb97f5faac70c5b6be3
Edgar Herrera <edgarherrera@hey.com>
2021-10-17 12:05:51 -0500

    Add main layout with animated background

---
+++ src/components/Layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "~/components/Header/Header";
+
+import styles from "./MainLayout.module.css";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const MainLayout = ({ children }: Props) => {
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <main className="container">{children}</main>
+    </div>
+  );
+};
