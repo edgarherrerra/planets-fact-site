de5bc964f5cedef2bfa97a76e6fd3394fecf706a
Edgar Herrera <edgarherrera@hey.com>
2021-10-20 15:26:59 -0500

    Add modal component to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { planetsIcons } from "~/lib/constants/icons";
 
 import { Icon } from "../common/Icon";
+import { Modal } from "../common/Modal/Modal";
 
 import styles from "./Header.module.css";
 
 export default function Header() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <header className={styles.header}>
       <span className={styles.title}>THE PLANETS</span>
-      <button>
+      <button onClick={() => setShowModal(!showModal)}>
         <Icon icon={planetsIcons.settings} size={24} />
       </button>
+      {showModal && <Modal />}
     </header>
   );
 }
