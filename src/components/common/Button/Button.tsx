86841c0a2f3e9faedf0e6f1f624601d82f5c794a
Edgar Herrera <edgarherrera@hey.com>
2021-10-20 13:04:25 -0500

    Update button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-function Button() {
+interface Props {
+  index: number;
+  title: string;
+}
+
+function Button({ index, title }: Props) {
+  const splitWords = title.split(" ");
+
   return (
     <button className={styles.buttonContainer}>
-      <span className={styles.indexItem}>01</span>
+      <span className={styles.indexItem}>0{index}</span>
       <span className={styles.labelContainer}>
-        <span className={styles.label}>INTERNAL </span>
-        <span>OVERVIEW</span>
+        {splitWords.length == 2 && (
+          <span className={styles.label}>{splitWords[1]} </span>
+        )}
+        <span>{splitWords[0]}</span>
       </span>
     </button>
   );
