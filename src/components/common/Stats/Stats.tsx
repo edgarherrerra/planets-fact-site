be4c3a1682f496afd9d7ac04251858f86930f842
Edgar Herrera <edgarherrera@hey.com>
2021-10-20 13:04:38 -0500

    Update stats component

--- src/components/common/Stats/Stats.tsx
+++ src/components/common/Stats/Stats.tsx
@@ -1,35 +1,24 @@
+import styles from "./Stats.module.css";
 
-import styles  from './Stats.module.css'
+interface Props {
+  data: [];
+}
 
-export const Stats = () => {
+export const Stats = ({ data }: Props) => {
   return (
     <>
       <ul className={styles.container}>
-        <li className={styles.listItem}>
-          <div className={styles.dataContainer}>
-            <span className={styles.title}>Rotation Time</span>
-            <span className={styles.data}>0.99 Days</span>
-          </div>
-        </li>
-        <li className={styles.listItem}>
-          <div className={styles.dataContainer}>
-            <span className={styles.title}>REVOLUTION TIME</span>
-            <span className={styles.data}>365.26 Days</span>
-          </div>
-        </li>
-        <li className={styles.listItem}>
-          <div className={styles.dataContainer}>
-            <span className={styles.title}>RADIUS</span>
-            <span className={styles.data}>6,371 KM</span>
-          </div>
-        </li>
-        <li className={styles.listItem}>
-          <div className={styles.dataContainer}>
-            <span className={styles.title}>AVERAGE TEMP</span>
-            <span className={styles.data}>16°c</span>
-          </div>
-        </li>
+        {data.map((element: any) => {
+          return (
+            <li key={element.id} className={styles.listItem}>
+              <div className={styles.dataContainer}>
+                <span className={styles.title}>{element.name}</span>
+                <span className={styles.data}>{element.data}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
-}
+};
